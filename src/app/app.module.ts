d690b03ea9e6f8e5c07b37c0fdc632a2aafa24dc
Sriram Nyshadham <nyshadhamramu@gmail.com>
2023-07-12 20:43:22 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AudioStreamer } from 'src/app/app.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  providers: [AudioStreamer],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
